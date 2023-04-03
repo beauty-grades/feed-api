33ba7406e5f4ee8dcc017f9171fddd142012fc09
Anthony Cueva <hi.cuevantngmail.com>
2023-04-02 21:18:34 -0500

    fixed multiple period for enrollments

---
+++ src/retry-fetch.js
@@ -0,0 +1,19 @@
+const MAX_RETRIES = 10;
+const RETRY_INTERVAL = 500;
+export async function retryFetch(url, options = {}, retries = 0) {
+  try {
+    const response = await fetch(url, options);
+    if (response.ok) {
+      return response;
+    }
+  } catch (error) {
+    console.error(`Failed to fetch in attemp ${retries}`);
+  }
+  if (retries < MAX_RETRIES) {
+    console.log(`Retrying fetch ${url} in ${RETRY_INTERVAL}ms`);
+    await new Promise((resolve) => setTimeout(resolve, RETRY_INTERVAL));
+    return retryFetch(url, options, retries + 1);
+  } else {
+    throw new Error(`Failed to fetch ${url} after ${MAX_RETRIES} retries`);
+  }
+}
